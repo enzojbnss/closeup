1bcabb10dfe994c301beae12ba667b5dd8ca4385
Antonio Marcos de Campos <enzojbnss@gmail.com>
2017-11-11 07:15:50 -0200

    first commit

---
+++ src/view/Page.php
@@ -0,0 +1,29 @@
+<?php
+namespace src\view;
+class Page{
+	
+	private $raiz;
+	private $corpo;
+	private $controller;
+	private $action;
+	
+	public function __construct($url,$base = "Layout.php"){
+				$folder = $url [0];		if($url [0]==null)$folder ="home";		$className = strtoupper(substr($folder, 0,1)).substr($folder, 1);		if(count($url)>1)$actionName = $url [1];		else $actionName = "index";		$this->corpo = "src/view/".$folder."/".$actionName.".php";		if($base==null)$this->raiz = $this->corpo;		else $this->raiz = "src/view/".$base;		$this->controller = $className;		$this->action = $actionName;
+	}
+	
+	public function getRaiz(){
+		return $this->raiz;
+	}
+	
+	public function getCorpo(){
+		return $this->corpo;
+	}
+	
+	public function getController(){
+		return $this->controller;
+	}
+	
+	public function getAction(){
+		return $this->action;
+	}
+}
